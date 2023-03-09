3c25c68786b968267629faf233241924b7933bab
ferchox920 <fernandoramones92@gmail.com>
2023-03-09 01:14:12 -0300

    commit

--- client/src/components/LandingPage/LandigPage.jsx
+++ client/src/components/LandingPage/LandigPage.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import styles from './LandingPage.module.css'
+import React from 'react';
 import { Link } from 'react-router-dom';
-import SearchInput from '../Search/SearchInput';
+import styles from './LandingPage.module.css';
 
-const LandigPage = () => {
+const LandingPage = () => {
   return (
-    <>
-    <Link to='/home'>LandigPage</Link>
+    <div className={styles.container}>
+      <main>
+        <h1 className={styles.title}>Bienvenidos a la página de reseñas de países</h1>
+        <p className={styles.description}>Explora nuestras reseñas y descubre todo lo que necesitas saber sobre diferentes países alrededor del mundo. Desde sus principales atracciones turísticas, hasta sus platos típicos y tradiciones. ¡Conviértete en un experto en viajes con nosotros!</p>
+        <Link to='/home' className={styles.ctaButton}>Comenzar a explorar</Link>
+      </main>
+      <footer className={styles.footer}>
+        <p className={styles.footerText}>Todos los derechos reservados © 2023</p>
+      </footer>
+    </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default LandigPage
+export default LandingPage;
